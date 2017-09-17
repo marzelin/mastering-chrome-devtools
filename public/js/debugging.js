4581a1989436c9089d92bcddc531ee6aca9b334a
marzelin <marzelin@gmail.com>
2017-09-17 23:29:16 +0200

    solve challenge 2

--- public/js/debugging.js
+++ public/js/debugging.js
@@ -1,34 +1,35 @@
-$("#github").on("submit", function() {
-  var user = $("#user");
+$("#github").on("submit", function(e) {
+  e.preventDefault()
+  var user = $("#user").val()
 
-  emptyCurrentList();
+  emptyCurrentList()
 
   // Using the github API https://developer.github.com/v3/repos/#list-user-repositories
   $.ajax({
-    url: "https://api.github.com/users/" + user + "/repos?client_id=1699520b1a1353e2d28f&client_secret=19f0b5d59aa52197d1c3e59ce8d85233738cda20",
+    url: "https://api.github.com/users/" + user + "/repos",
     success: function(repos) {
-      iterateThroughData(repos);
+      iterateThroughData(repos)
     },
     error: function(data) {
-      insertError();
+      insertError()
     }
-  });
-});
+  })
+})
 
 function iterateThroughData(repos) {
   _.forEachRight(repos, function(repo) {
-    insertHTML(repo);
-  });
+    insertHTML(repo)
+  })
 }
 
 function insertHTML(repo) {
-  $("#repositories").append("<li>" + repo.title + "</li>");
+  $("#repositories").append("<li>" + repo.name + "</li>")
 }
 
 function emptyCurrentList() {
-  $("#repositories").empty();
+  $("#repositories").empty()
 }
 
 function insertError() {
-  $("#repositories").append("<li>Uh oh, we're having a problem!</li>");
+  $("#repositories").append("<li>Uh oh, we're having a problem!</li>")
 }
